81f965bf8078c15ef75880b2fdb450f395608137
goktugocal <goktugocal41@gmail.com>
2023-06-05 02:44:37 +0300

    debug3

--- src/helpers/chatbot_bard.js
+++ src/helpers/chatbot_bard.js
@@ -37,7 +37,7 @@ class Chatbot {
         if (resp.status !== 200) {
             throw new Error('Could not get Google Bard');
         }
-        console.log(resp);
+        console.log(resp.match(/"SNlM0e":"(.*?)"/));
         const SNlM0e = resp.data.match(/"SNlM0e":"(.*?)"/)[1];
 
         return SNlM0e;
